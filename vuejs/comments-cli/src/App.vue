fe03a6bd53738193e6053449a243d5ffbcb0090b
Guilherme Chaves <g.candido.chaves@avanade.com>
2020-07-12 13:13:41 -0300

    Built comments component

--- vuejs/comments-cli/src/App.vue
+++ vuejs/comments-cli/src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Comments />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Comments from './components/Comments.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Comments
   }
 }
 </script>
